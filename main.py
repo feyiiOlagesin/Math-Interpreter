d755a158b87110f3d90e166ce8751a8dbdaa3a0d
Olagesin Samuel <feyisamuell@gmail.com>
2020-05-17 00:31:12 +0100

    Added interpreter to interpret the trees

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from lexer import Lexer
 from parser_ import Parser 
+from interpreter import Interpreter
 
 while True:
-    text = input("Math Interpreter> ")
-    lexer = Lexer(text)
-    tokens = lexer.generate_tokens()
-    parser = Parser(tokens)
-    tree = parser.parse()
-    print(tree)
+    try:
+        text = input("Math Interpreter> ")
+        lexer = Lexer(text)
+        tokens = lexer.generate_tokens()
+        parser = Parser(tokens)
+        tree = parser.parse()
+        if not tree:
+            continue
+        else:
+            interpreter = Interpreter()
+            value = interpreter.visit(tree)
+            print(value)
+    except Exception as e:
+        print(e)
     # print(list(tokens)) 
